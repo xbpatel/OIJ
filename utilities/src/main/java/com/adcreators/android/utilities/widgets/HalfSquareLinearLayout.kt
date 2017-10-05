74f2a42cb769da01d99af064d21a2991a8b830d8
dhruvil <dhruvil@adcreators.com.au>
2017-10-05 17:08:02 +0530

    Tab icons and pager adapter

---
+++ utilities/src/main/java/com/adcreators/android/utilities/widgets/HalfSquareLinearLayout.kt
@@ -0,0 +1,34 @@
+package com.adcreators.android.utilities.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * Created by excellent-3 on 22/08/17.
+ */
+open class HalfSquareLinearLayout : LinearLayout {
+
+
+     constructor(context : Context) : super(context) {
+
+
+     }
+
+     constructor(context : Context , attrs : AttributeSet) : super(context , attrs) {
+
+     }
+
+
+     public override fun onMeasure(widthMeasureSpec : Int , heightMeasureSpec : Int) {
+
+          super.onMeasure(widthMeasureSpec , widthMeasureSpec)
+
+          val width = MeasureSpec.getSize(widthMeasureSpec)
+          val height = MeasureSpec.getSize(heightMeasureSpec)
+          val size = if (width > height) height else width
+          setMeasuredDimension(size , (size/2))
+
+     }
+
+}
